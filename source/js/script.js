c5cd8d0d5a642f16026193036f64a64c89b0a747
Anna Levchenko <levallevchenko@gmail.com>
2021-05-23 20:53:54 +0300

    add fonts

--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,1 @@
-// var navMain = document.querySelector(".main-nav");
-// var navMainList = document.querySelector(".main-nav__list");
-// var navToggle = document.querySelector(".main-nav__burger");
 
-// navMain.classList.remove("main-nav--nojs");
-
-// navToggle.addEventListener("click", function () {
-//   if (navMain.classList.contains("main-nav--closed")) {
-//     navMain.classList.remove("main-nav--closed");
-//     navMainList.classList.remove("main-nav__list--closed");
-//     navMain.classList.add("main-nav--opened");
-//   } else {
-//     navMain.classList.add("main-nav--closed");
-//     navMainList.classList.add("main-nav__list--closed");
-//     navMain.classList.remove("main-nav--opened");
-//   }
-// });
